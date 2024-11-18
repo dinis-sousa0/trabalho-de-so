0eba9a594903208503050c7797c9cb94f263ed2d
dinis <dinis.sousa@ua.pt>
2024-11-18 16:32:47 +0000

    compos

--- backup_check.sh
+++ backup_check.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# Verificación de argumentos
-if [[ "$#" -lt 2 ]]; then
-  printf "Uso: %s <dir_trabalho> <dir_backup>\n" "$0"
+#verificaoes
+if [[ "$#" -lt 2 ]]; then #numero argumentos
+  printf "Por favor escreva: %s 1 2\n" "$0"
   exit 1
 fi
 
-# Función para comparar hashes de archivos
+#comparar hash dos arquivos
 verifica_diferenca() {
   local arquivo_trabalho="$1"
   local arquivo_backup="$2"
 
-  # Calcular los hashes MD5
+  #calcular hash MD5
   local md5_trabalho
   local md5_backup
   md5_trabalho=$(md5sum "$arquivo_trabalho" | awk '{print $1}')
   md5_backup=$(md5sum "$arquivo_backup" | awk '{print $1}')
 
-  # Comparar hashes
+  #comparar
   if [[ "$md5_trabalho" != "$md5_backup" ]]; then
-    printf "DIFERENTE: %s y %s\n" "$arquivo_trabalho" "$arquivo_backup"
+    printf "%s %s differ.\n" "$arquivo_trabalho" "$arquivo_backup"
   else
     printf "%s %s iguais\n" "$arquivo_trabalho" "$arquivo_backup"
   fi
 }
 
-# Recorrer los archivos del directorio de trabajo
+#recorrer
 find "$1" -type f | while read -r arquivo_trabalho; do
-  # Construir la ruta correspondiente en el directorio de backup
+  #caminho correspondente
   arquivo_backup="${2}${arquivo_trabalho#$1}"
 
-  # Comparar solo si el archivo en el backup existe
+  #comparar se existe
   if [[ -f "$arquivo_backup" ]]; then
     verifica_diferenca "$arquivo_trabalho" "$arquivo_backup"
   fi
